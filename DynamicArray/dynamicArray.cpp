3b35b0eee2ae8262856208c876009166e95b53e8
alexishinse <alexishinse@hotmail.com>
2015-01-30 10:40:26 -0500

    Lab 2 commit #3, 3 test réussi

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,9 +1,23 @@
 #include "dynamicArray.h"
 
+DynamicArray::DynamicArray(){
+	tabElements = new int[100];
+
+	for (int i = 0; i < 100; i++){
+		tabElements[i] = 0;
+	}
+	
+	capacite = 100;
+}
+
 unsigned int DynamicArray::getCapacite(){
-	return 100;
+	return capacite;
 }
 
 int DynamicArray::getElement(unsigned int _index){
-	return NULL;
+	return tabElements[_index];
+}
+
+void DynamicArray::setElement(unsigned int _index, int _valeur){
+	tabElements[_index] = _valeur;
 }
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -7,13 +7,14 @@ class DynamicArray
 {
 
 public:
+	DynamicArray();
 	unsigned int getCapacite();
 	int getElement(unsigned int _index);
-
+	void setElement(unsigned int _index, int _valeur);
 
 private:
-
-
+	unsigned int capacite;
+	int * tabElements;
 };
 
 
